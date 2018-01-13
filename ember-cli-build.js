ce10564894d538bd79dac2ca676f4a3467939550
snewcomer24 <snewcomer24@gmail.com>
2018-01-12 18:06:02 -0800

    update build schtuff

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,8 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     'ember-font-awesome': {
-      fontFormats: ['woff', 'woff2']
+      fontFormats: ['woff', 'woff2'],
+      includeFontFiles: false
     }
   });
 
